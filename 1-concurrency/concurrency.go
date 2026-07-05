807abf2463e3507f5e712bee92c09adf08a107db
TaranyukK <taranyuk.kirill@gmail.com>
2026-07-06 00:24:26 +0300

    edit functions

--- 1-concurrency/concurrency.go
+++ 1-concurrency/concurrency.go
@@ -5,7 +5,7 @@ import "math/rand/v2"
 func CreateSlice(out chan<- int) {
 	numbers := []int{}
 	for range 10 {
-		numbers = append(numbers, rand.IntN(100))
+		numbers = append(numbers, rand.IntN(101))
 	}
 	for _, n := range numbers {
 		out <- n
@@ -13,7 +13,9 @@ func CreateSlice(out chan<- int) {
 	close(out)
 }
 
-func PowNumber(number int, out chan<- int) {
-	out <- number * number
+func PowNumber(in <-chan int, out chan<- int) {
+	for number := range in {
+		out <- number * number
+	}
 	close(out)
 }
--- main.go
+++ main.go
@@ -3,21 +3,14 @@ package main
 import (
 	"fmt"
 	concur "go/concurrency/1-concurrency"
-	"sync"
 )
 
 func main() {
-	var wg sync.WaitGroup
 	generatedNum := make(chan int)
 	powedNum := make(chan int)
-	wg.Go(func() {
-		concur.CreateSlice(generatedNum)
-	})
-	for val := range generatedNum {
-		wg.Go(func() {
-			concur.PowNumber(val, powedNum)
-		})
-	}
+	go concur.CreateSlice(generatedNum)
+	go concur.PowNumber(generatedNum, powedNum)
+
 	for val := range powedNum {
 		fmt.Println(val)
 	}
